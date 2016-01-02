a51d4380d734d278b86ee13be5d204db3162e014
Wendy Smoak <wsmoak@gmail.com>
2016-01-02 21:53:29 +0000

    Add GenServers for proximity sensor and servo

---
+++ lib/cat_feeder/proximity_worker.ex
@@ -0,0 +1,16 @@
+defmodule CatFeeder.ProximityWorker do
+  use GenServer
+
+  # Server (callbacks)
+
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  # Client
+
+
+
+  # Helper Functions
+
+end
