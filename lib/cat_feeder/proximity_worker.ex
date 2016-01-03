5e8c9a724dcbe182023276d4dc7d0ea29510a19e
Wendy Smoak <wsmoak@gmail.com>
2016-01-03 00:59:29 +0000

    An attempt using Task.async

--- lib/cat_feeder/proximity_worker.ex
+++ lib/cat_feeder/proximity_worker.ex
@@ -1,8 +1,11 @@
 defmodule CatFeeder.ProximityWorker do
+  require Logger
   use GenServer
 
 # register        address
   @cmd            0x80
+  @prox_result_h  0x87
+  @prox_result_l  0x88
 
   # Client
 
@@ -14,9 +17,11 @@ defmodule CatFeeder.ProximityWorker do
 
   def init(_opts) do
 
-    {:ok,prox_pid} = I2c.start_link("i2c-1", 0x13)
+    {:ok,pid} = I2c.start_link("i2c-1", 0x13)
     # turn on proximity sensing by setting bits 0 and 1
-    I2c.write(prox_pid, <<@cmd, 0x03>> )
+    I2c.write(pid, <<@cmd, 0x03>> )
+
+    Task.async( __MODULE__, :check_proximity, [pid] )
 
     {:ok, %{state: :idle}}
 
@@ -29,5 +34,21 @@ defmodule CatFeeder.ProximityWorker do
   end
 
   # Helper Functions
+
+  def check_proximity(pid) do
+    << val :: 16 >> = I2c.write_read(pid,<<@prox_result_h>> ,2) 
+    Logger.debug "Proximity value #{val}" 
+ 
+    # value determined by running measure/1 and observing the output
+    if val > 2100 do
+      Logger.debug "Sending message to feed the cat!"  
+      #Gpio.write(led_pid,1)
+      {:ok, %{state: :waiting}}
+    else
+      Logger.debug "nobody around..."
+      #Gpio.write(led_pid,0)
+      {:ok, %{state: :idle}}
+    end
+  end
 
 end
